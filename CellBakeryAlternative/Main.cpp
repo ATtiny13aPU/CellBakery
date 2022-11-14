0d103ca0c2625b93882646faea3933596c72de53
ATtiny13aPU <ATtiny13aPU@gmail.com>
2022-11-14 15:02:33 +0400

    Исправление ошибок

--- CellBakeryAlternative/Main.cpp
+++ CellBakeryAlternative/Main.cpp
@@ -4,7 +4,7 @@ using namespace osl;
 
 class key_trigger {
 public:
-	void push (bool t) {
+	void push(bool t) {
 		if (t) {
 			is_click = !is_press;
 			is_press = true;
@@ -41,6 +41,7 @@ private:
 	shad::ComputeShader iniComputeShader;
 
 	shad::SSBO CellsSSBO;
+	shad::SSBO GridSSBO;
 
 	uint32_t count_of_frames = -1;
 	std::ostringstream buff; // текст буффер
@@ -67,8 +68,13 @@ private:
 		float angle;
 		fvec3 color_rgb;
 		fvec3 color_hsv;
-		int type_id;
-		int linked_list;
+		int32_t type_id;
+		int32_t linked_list;
+	};
+
+	struct Chunk_ssboBlock {
+		int32_t linked_list;
+		float brightness;
 	};
 };
 
@@ -81,7 +87,7 @@ int Context::run() {
 	//glfwSetKeyCallback(window, )
 
 	{
-		std::vector<float> m = {-1., -1., -1., 1., 1., -1., 1., 1.};
+		std::vector<float> m = { -1., -1., -1., 1., 1., -1., 1., 1. };
 		fullScreenMesh.loadFrom(&m[0], m.size());
 	}
 
@@ -91,10 +97,10 @@ int Context::run() {
 		petriShader.name = std::string("petriShader");
 		std::ifstream inpf;
 		inpf.open("Shaders/petri.vert");
-		std::string sourseV {std::istreambuf_iterator<char>(inpf), std::istreambuf_iterator<char>()};
+		std::string sourseV{ std::istreambuf_iterator<char>(inpf), std::istreambuf_iterator<char>() };
 		inpf.close();
 		inpf.open("Shaders/petri.frag", std::ios_base::in);
-		std::string sourseF {std::istreambuf_iterator<char>(inpf), std::istreambuf_iterator<char>()};
+		std::string sourseF{ std::istreambuf_iterator<char>(inpf), std::istreambuf_iterator<char>() };
 		inpf.close();
 		petriShader.compile(sourseV.c_str(), sourseF.c_str());
 	}
@@ -103,13 +109,13 @@ int Context::run() {
 		cellsShader.name = std::string("cellsShader");
 		std::ifstream inpf;
 		inpf.open("Shaders/cells.vert");
-		std::string sourseV {std::istreambuf_iterator<char>(inpf), std::istreambuf_iterator<char>()};
+		std::string sourseV{ std::istreambuf_iterator<char>(inpf), std::istreambuf_iterator<char>() };
 		inpf.close();
 		inpf.open("Shaders/cells.frag", std::ios_base::in);
-		std::string sourseF {std::istreambuf_iterator<char>(inpf), std::istreambuf_iterator<char>()};
+		std::string sourseF{ std::istreambuf_iterator<char>(inpf), std::istreambuf_iterator<char>() };
 		inpf.close();
 		inpf.open("Shaders/cells.geom", std::ios_base::in);
-		std::string sourseG {std::istreambuf_iterator<char>(inpf), std::istreambuf_iterator<char>()};
+		std::string sourseG{ std::istreambuf_iterator<char>(inpf), std::istreambuf_iterator<char>() };
 		inpf.close();
 		cellsShader.compile(sourseV.c_str(), sourseF.c_str(), sourseG.c_str());
 	}
@@ -145,33 +151,10 @@ int Context::run() {
 	// Создание мира (ВНИМАНИЕ! Данные мира больше не используются, все вычисления переносятся на GPU)
 	///==============================
 	WorldCS world;
-	world.setup.Dp = 8.;
-	int Counter1 = 0;
+	world.Dp = 8.;
 
-	world.setup.name = "Test world";
-	std::thread TWT(&WorldCS::RenderWorld, &world);
-	TWT.detach();
-	while (!world.control.out_ready) std::this_thread::sleep_for(std::chrono::milliseconds(1));
+	world.iniWorld();
 
-
-	GLuint light_map = 0; // текстура карты мира для яркости света
-	{
-		glGenTextures(1, &light_map);
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, light_map);
-
-		// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-		// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST); // без интерполяции
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE); // выдаёт цвет ближайшего пикселя в пределах текстуры при вылете за границы
-
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_R8, world.setup.out_mapSize, world.setup.out_mapSize, 0, GL_RED, GL_UNSIGNED_BYTE, NULL);
-
-		glBindImageTexture(0, light_map, 0, GL_FALSE, 0, GL_READ_WRITE, GL_R8);
-	}
 
 	//glEnable(GL_MULTISAMPLE);
 
@@ -198,73 +181,13 @@ int Context::run() {
 		glClear(GL_COLOR_BUFFER_BIT);
 		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-
-		static key_trigger boost;
-		boost.push(glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS);
-		static key_trigger test;
-		test.push(glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS);
-		static key_trigger gmesh;
-		gmesh.push(glfwGetKey(window, GLFW_KEY_G) == GLFW_PRESS);
-
-
-		// Вычисления
+		// Управление камерой мышью и клавой
+		static key_trigger boost, test, gmesh;
+		vec2 mnPos, mxPos;
 		{
-
-			// Инициализация
-			if (count_of_frames == -1) {
-				CellsSSBO.setSize(world.setup.set_max_ec * sizeof(Cell_ssboBlock));
-				CellsSSBO.bind(iniComputeShader.glID, "ssbo_cells");
-				glUseProgram(iniComputeShader.glID);
-				static bool first_call = 1;
-				if (first_call) {
-					first_call = 0;
-					glUniform1i(glGetUniformLocation(iniComputeShader.glID, "mapSize"), world.setup.out_mapSize);
-					glUniform1f(glGetUniformLocation(iniComputeShader.glID, "mst"), world.view.mst);
-					glUniform1f(glGetUniformLocation(iniComputeShader.glID, "Dp"), world.setup.Dp);
-					glUniform1f(glGetUniformLocation(iniComputeShader.glID, "Ac"), world.setup.Ac);
-				}
-
-				glDispatchCompute(world.setup.set_max_ec, 1, 1);
-				glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
-
-				CellsSSBO.bind(listComputeShader.glID, "ssbo_cells");
-				CellsSSBO.bind(cellsShader.glID, "ssbo_cells");
-			}
-
-			// свет
-			if (count_of_frames == -1) {
-				glUseProgram(lightComputeShader.glID);
-				static bool first_call = 1;
-				if (first_call) {
-					first_call = 0;
-					glUniform1f(glGetUniformLocation(lightComputeShader.glID, "mapSize"), float(world.setup.out_mapSize));
-				}
-
-				glActiveTexture(GL_TEXTURE0);
-				glBindTexture(GL_TEXTURE_2D, light_map);
-
-				glDispatchCompute(world.setup.out_mapSize, world.setup.out_mapSize, 1);
-
-				//glMemoryBarrier(GL_ALL_BARRIER_BITS);
-				glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
-			}
-
-			if (test.is_press) {
-				glUseProgram(listComputeShader.glID);
-				static bool first_call = 1;
-				if (first_call) {
-					first_call = 0;
-					glUniform1f(glGetUniformLocation(lightComputeShader.glID, "mapSize"), float(world.setup.out_mapSize));
-				}
-
-				glDispatchCompute(8, 8, world.setup.set_max_ec / 1024);
-
-				glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
-			}
-		}
-
-		// Графика
-		{
+			boost.push(glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS);
+			test.push(glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS);
+			gmesh.push(glfwGetKey(window, GLFW_KEY_G) == GLFW_PRESS);
 			// управление камерой
 			{	///==============================
 				// движение клавиатурой
@@ -279,7 +202,7 @@ int Context::run() {
 						world.view.Pos[1] += (frameTime.get() / 1000.) * world.view.mst;
 				}	///==============================
 
-				
+
 				// масштабирование в указатель и движение
 				{	///==============================
 					// получение эвента колёсика мыши
@@ -309,7 +232,7 @@ int Context::run() {
 						world.view.Pos = mix(mnPos, mxPos, 0.5);
 						world.view.mst = mnPos[1] - mxPos[1];
 
-						world.view.mst = std::max(world.view.mst, 0.00001 * world.setup.Dp);
+						world.view.mst = std::max(world.view.mst, 0.00001 * world.Dp);
 					}
 					while (scroll < -0.01) { // отдаление 
 						scroll += 0.1;
@@ -320,34 +243,96 @@ int Context::run() {
 						world.view.Pos = mix(mnPos, mxPos, 0.5);
 						world.view.mst = mnPos[1] - mxPos[1];
 
-						world.view.mst = std::min(world.view.mst, world.setup.Dp * 2.);
+						world.view.mst = std::min(world.view.mst, world.Dp * 2.);
 					}
 				}	///==============================
-				
+
 			}	///==============================
 
-			vec2 mnPos = mPos_to_wPos(vec2(0, 0), world.view.mst, world.view.Pos);
-			vec2 mxPos = mPos_to_wPos(vec2(Xd, Yd), world.view.mst, world.view.Pos);
+			mnPos = mPos_to_wPos(vec2(0, 0), world.view.mst, world.view.Pos);
+			mxPos = mPos_to_wPos(vec2(Xd, Yd), world.view.mst, world.view.Pos);
+		}
 
+		// Вычисления
+		{
+			// Инициализация ssbo буферов
+			if (count_of_frames == -1) {
+
+				CellsSSBO.setSize(world.mec * sizeof(Cell_ssboBlock));
+
+				CellsSSBO.bind(iniComputeShader.glID, "ssbo_cells");
+				CellsSSBO.bind(listComputeShader.glID, "ssbo_cells");
+				CellsSSBO.bind(cellsShader.glID, "ssbo_cells");
+
+
+				GridSSBO.setSize(world.Dm * world.Dm * (sizeof(Chunk_ssboBlock) + 4));
+
+				GridSSBO.bind(listComputeShader.glID, "ssbo_grid");
+				GridSSBO.bind(lightComputeShader.glID, "ssbo_grid");
+				GridSSBO.bind(petriShader.glID, "ssbo_grid");
+			}
+
+			// инициализатор клеток
+			if (count_of_frames == -1) {
+				glUseProgram(iniComputeShader.glID);
+				static bool first_call = 1;
+				if (first_call) {
+					first_call = 0;
+					glUniform1f(glGetUniformLocation(iniComputeShader.glID, "Dp"), world.Dp);
+					glUniform1f(glGetUniformLocation(iniComputeShader.glID, "Ac"), world.Ac);
+				}
+		
+				glDispatchCompute(world.mec, 1, 1);
+				glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
+			}
+
+			// вычисление света
+			if (count_of_frames == -1) {
+				glUseProgram(lightComputeShader.glID);
+				static bool first_call = 1;
+				if (first_call) {
+					first_call = 0;
+					glUniform1i(glGetUniformLocation(lightComputeShader.glID, "Dm"), world.Dm);
+				}
+
+				glDispatchCompute(world.Dm, world.Dm, 1);
+
+				//glMemoryBarrier(GL_ALL_BARRIER_BITS);
+				glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
+			}
+
+			// физика клеток
+			if (test.is_press) {
+				glUseProgram(listComputeShader.glID);
+				static bool first_call = 1;
+				if (first_call) {
+					first_call = 0;
+				}
+
+				glDispatchCompute(8, 8, world.mec / 1024);
+
+				glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
+			}
+		}
+
+		// Графика
+		{
 			// чашка петри
 			if (1) {
 				glUseProgram(petriShader.glID);
+				static bool first_call = 1;
+				if (first_call) {
+					first_call = 0;
+					glUniform1i(glGetUniformLocation(petriShader.glID, "Dm"), world.Dm);
+					glUniform1f(glGetUniformLocation(petriShader.glID, "Dp"), world.Dp);
+					glUniform1f(glGetUniformLocation(petriShader.glID, "Ac"), world.Ac);
+				}
 				glUniform4f(glGetUniformLocation(petriShader.glID, "ViewWorld"), mnPos[0], mxPos[1], mxPos[0], mnPos[1]);
 				glUniform2i(glGetUniformLocation(petriShader.glID, "WinSize"), Xd, Yd);
-				glUniform1i(glGetUniformLocation(petriShader.glID, "mapSize"), world.setup.out_mapSize);
 				glUniform2f(glGetUniformLocation(petriShader.glID, "cPos"), world.view.Pos[0], world.view.Pos[1]);
 				glUniform2f(glGetUniformLocation(petriShader.glID, "mPos"), mPos[0], mPos[1]);
 				glUniform1f(glGetUniformLocation(petriShader.glID, "mst"), world.view.mst);
-				glUniform1f(glGetUniformLocation(petriShader.glID, "Dp"), world.setup.Dp);
-				glUniform1f(glGetUniformLocation(petriShader.glID, "Ac"), world.setup.Ac);
-				//glUniform1i(glGetUniformLocation(petriShader.glID, "MSAA"), MSAAuniform[MSAA]);
 
-				// glBindTexture(GL_TEXTURE_2D, light_map); // обновление данных карта света
-				// glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, world.setup.out_mapSize, world.setup.out_mapSize, GL_RED, GL_UNSIGNED_BYTE, world.light_map);
-
-				glActiveTexture(GL_TEXTURE0);
-				glBindTexture(GL_TEXTURE_2D, light_map);
-				glUniform1i(glGetUniformLocation(petriShader.glID, "Light"), 0);
 
 				glBindVertexArray(fullScreenMesh.VAO);
 				for (int i = -1; i < 50 * boost.is_press; i++) {
@@ -355,50 +340,54 @@ int Context::run() {
 				}
 			}
 			// клетки
-			if (1) {
-				
+			if (0) {
+
 				glUseProgram(cellsShader.glID);
+				static bool first_call = 1;
+				if (first_call) {
+					first_call = 0;
+					glUniform1i(glGetUniformLocation(cellsShader.glID, "mapSize"), world.Dm);
+					glUniform1f(glGetUniformLocation(cellsShader.glID, "Dp"), world.Dp);
+					glUniform1f(glGetUniformLocation(cellsShader.glID, "Ac"), world.Ac);
+				}
 				glUniform4f(glGetUniformLocation(cellsShader.glID, "ViewWorld"), mnPos[0], mxPos[1], mxPos[0], mnPos[1]);
 				glUniform2i(glGetUniformLocation(cellsShader.glID, "WinSize"), Xd, Yd);
-				glUniform1i(glGetUniformLocation(cellsShader.glID, "mapSize"), world.setup.out_mapSize);
 				glUniform2f(glGetUniformLocation(cellsShader.glID, "cPos"), world.view.Pos[0], world.view.Pos[1]);
 				glUniform2f(glGetUniformLocation(cellsShader.glID, "mPos"), mPos[0], mPos[1]);
 				glUniform1f(glGetUniformLocation(cellsShader.glID, "mst"), world.view.mst);
-				glUniform1f(glGetUniformLocation(cellsShader.glID, "Dp"), world.setup.Dp);
-				glUniform1f(glGetUniformLocation(cellsShader.glID, "Ac"), world.setup.Ac);
 				glUniform1f(glGetUniformLocation(cellsShader.glID, "fTime"), count_of_frames / 60.);
 				glUniform1i(glGetUniformLocation(cellsShader.glID, "GM"), gmesh.is_press);
-				
-				
+
+
 				glEnable(GL_BLEND);
 				glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 				glBindVertexArray(voidMesh.VAO);
 				for (int i = -1; i < 50 * boost.is_press; i++) {
-					glDrawArraysInstanced(GL_POINTS, 0, voidMesh.size, world.setup.set_max_ec); //world.setup.set_max_ec
+					glDrawArraysInstanced(GL_POINTS, 0, voidMesh.size, world.mec); //world.set_max_ec
 				}
 				glDisable(GL_BLEND);
 			}
 		}
-		
-		count_of_frames++;
-		buff.str("");
-		bool swapBit = count_of_frames % 2;
-		frame_time_point[swapBit] = chGetTime();
-		frameTime.push(chDurationMillis(frame_time_point[swapBit], frame_time_point[!swapBit]));
-		if (count_of_frames % 100000 == 0)
-			frameTime.resum();
 
-		buff << "fps - " << std::fixed << std::setprecision(1) << 1000. / frameTime.get();
-		buff << "  ms - " << std::fixed << std::setprecision(2) << frameTime.get();
-		buff << "         " << std::fixed << std::setprecision(1) << count_of_frames / 60.;
-		glfwSetWindowTitle(window, buff.str().c_str());
+		// Подсчёт кадров
+		{
+			count_of_frames++;
+			buff.str("");
+			bool swapBit = count_of_frames % 2;
+			frame_time_point[swapBit] = chGetTime();
+			frameTime.push(chDurationMillis(frame_time_point[swapBit], frame_time_point[!swapBit]));
+			if (count_of_frames % 100000 == 0)
+				frameTime.resum();
+
+			buff << "fps - " << std::fixed << std::setprecision(1) << 1000. / frameTime.get();
+			buff << "  ms - " << std::fixed << std::setprecision(2) << frameTime.get();
+			buff << "         " << std::fixed << std::setprecision(1) << count_of_frames / 60.;
+			glfwSetWindowTitle(window, buff.str().c_str());
+		}
 
 		glfwSwapBuffers(window);
 	}
 
-	world.control.shutdown();
-	while (!world.control.out_safe_shutdown)
-		std::this_thread::sleep_for(std::chrono::milliseconds(1));
 	return 1;
 }
 
--- CellBakeryAlternative/world.h
+++ CellBakeryAlternative/world.h
@@ -23,132 +23,34 @@ enum CellType {
 	Cilio		// Цилиоцит
 };
 
-class CellCS {
-public:
-	frac angle;
-	frac r;
-	vec2 pos;
-	ivec2 ipos;
-	int32_t mapID;
-	int32_t buffID;
-	int32_t buffID2;
-	int32_t buffCount;
-	vec3 color;
-	uint8_t type; // CellType
-
-	CellCS() {
-		pos = vec2(0.);
-		r = 0.;
-	}
-private:
-};
-
 class WorldCS {
 public:
 	struct View {
 		vec2 Pos = vec2(0, 0); // Позиция центра камеры (0 - центр субстрата)
-		frac mst = 1.; // Масштаб зрения (чем больше, тем меньше выглядит субстрат)
+		frac mst = 1.02; // Масштаб зрения (чем больше, тем меньше выглядит субстрат)
 	};
 	View view;
-	class Setup {
-	public:
-		std::string name = "Unnamed";
-		frac Dp; // Диаметр чаши в мм при создании
-		frac Ac = 0.03; // Размер сетки мира (не трогать внешними методами, только для чтения)
-		int out_mapSize = 1;
-		int out_maxСollisionData = 1;
-		int set_max_ec = 100000;
-		int set_max_efp = 10000;
-		uint64_t main_seed = 0x12067B01A37EE0E2;
-	};
-	Setup setup;
-	class Control {
-	public:
-		int out_ready = 0; // готовность данных мира к использованию внешним кодом
-		int out_safe_shutdown = 0; // готовность к выключению потока мира
-		int out_run_step = 0; // колво запрошенных шагов в очереди
-		bool in_auto_run_mode = 0; // после первого вызова world.run_step(1); мир будет обрабатываться автономно не дожидаясь ответа
-		uint64_t out_counter_of_step = 0; // колво шагов с момента старта симуляции
 
-		void shutdown() { p_shutdown = 1; } // указать завершение работы
-
-		int out_debug1 = 0; // буфферная переменная для дебага
-
-		int p_start = 0;
-		int p_shutdown = 0;
-	};
-	Control control;
+	std::string name = "Unnamed";
+	frac Dp; // Диаметр чаши в мм при создании
+	frac Ac = 0.03; // Размер сетки мира
+	int Dm = 1;
+	int mec = 100000; // максимальное число клеток
+	int mefp = 10000; // максимальное число свободных частиц
 	
-	void RenderWorld();
-	void RunWorld();
-	void newCell(int);
-	bool newCell(vec2, frac);
-	void newCell();
+	void iniWorld();
 
-	double debugF1, debugF2, debugF3;
 private:
-	Randomaizer RAND;
-	int ec = 0;
-	int mec;
-	int mefp;
-	frac Rp, Dp, Rc, Ac, hdAc;
 };
 
-void WorldCS::RunWorld() {
-}
-
-
-void WorldCS::RenderWorld() {
-	RAND.ini(setup.main_seed);
-	std::ostringstream buff;
-
-	Rp = setup.Dp / 2.; // Радиус чаши в мм
-	Dp = Rp * 2; // Диаметр чаши в мм
+void WorldCS::iniWorld() {
+	frac Rp = Dp / 2.;
 	view.mst *= Dp;
 	view.Pos = vec2(Dp / 2.);
 
-	int Dm = floor(Dp / setup.Ac / 4.) * 4;
+	Dm = ceil(Dp / Ac / 1.) * 1;
 
-	Rc = setup.Ac;
-	setup.Ac = Dp / Dm;
-	Ac = setup.Ac;
-	hdAc = 1. / Ac;
-	setup.out_mapSize = Dm;
+	Ac = Dp / Dm;
 
-
-	mec = setup.set_max_ec;
-	setup.set_max_ec = ceil(mec / 1024.) * 1024;
-	mec = setup.set_max_ec;
-	setup.out_maxСollisionData = ceil(mec * 20. / 4096.) * 4096;
-	int mecl = setup.out_maxСollisionData;
-	
-	mefp = setup.set_max_efp;
-
-	std::chrono::time_point<std::chrono::steady_clock> benchPoint[10];
-	benchPoint[0] = std::chrono::steady_clock::now();
-
-	benchPoint[3] = std::chrono::steady_clock::now();
-
-	debugF1 = std::chrono::duration<double, std::milli>(benchPoint[1] - benchPoint[0]).count();
-	debugF2 = std::chrono::duration<double, std::milli>(benchPoint[2] - benchPoint[1]).count();
-	debugF3 = std::chrono::duration<double, std::milli>(benchPoint[3] - benchPoint[2]).count();
-
-	control.out_ready = 1; // разрешаем графике идти дальше
-	// главный цикл рендера мира
-	while (control.p_shutdown == 0) {
-
-		if (control.out_run_step == 0)
-			std::this_thread::sleep_for(std::chrono::microseconds(10));
-
-		while (control.out_run_step > 0) {
-			benchPoint[1] = std::chrono::steady_clock::now();
-			benchPoint[2] = std::chrono::steady_clock::now();
-
-			debugF3 = std::chrono::duration<double, std::milli>(benchPoint[2] - benchPoint[1]).count();
-
-			control.out_counter_of_step++;
-			control.out_run_step--;
-		}
-	}
-	control.out_safe_shutdown = 1;
+	mec = ceil(mec / 1024.) * 1024;
 }
