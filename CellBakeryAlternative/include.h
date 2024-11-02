9d7ff2e2f21d30a0c0c8c7326aa5aeeeaeb9d996
ATtiny13aPU <82825988+ATtiny13aPU@users.noreply.github.com>
2024-11-02 22:00:33 +0400

    Отчистка проекта. Начало реставрации.

--- CellBakeryAlternative/include.h
+++ CellBakeryAlternative/include.h
@@ -5,25 +5,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#define STB_IMAGE_IMPLEMENTATION
-#include "OpenGL/stb_image.h"
+#include <OSL/include.h>
+
 #define NOMINMAX
-
-#include "OpenGL/gl_func.h"
-#include "OpenGL/shad_primitive.h"
-
-#include <vector>
-#include <fstream>
-#include <thread>
-#include <cmath>
-#include <ctime>
-#include <sstream>
-#include <algorithm>
-#include <iomanip>
-#include <array>
-
-
-#include "Vector.hpp"
-#include "OSL/include.h"
-
-#include "world.h"
+#undef min
+#undef max
