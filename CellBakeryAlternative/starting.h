906befcc6b91891621ae978bef812c5a7ba2be45
ATtiny13aPU <82825988+ATtiny13aPU@users.noreply.github.com>
2024-11-02 23:16:11 +0400

    Добавлена проверка на изменение размеров окна.

--- CellBakeryAlternative/starting.h
+++ CellBakeryAlternative/starting.h
@@ -18,8 +18,12 @@ int Context::run() {
 			glfwPollEvents();
 
 			// Ќастраиваем камеру под разрешение окна
-			glfwGetFramebufferSize(window, reinterpret_cast<int*>(&winSize[0]), reinterpret_cast<int*>(&winSize[1]));
-			glViewport(0, 0, winSize[0], winSize[1]);
+			int new_xsize_display, new_ysize_display;
+			glfwGetFramebufferSize(window, &new_xsize_display, &new_ysize_display);
+			if (uint32_t(new_xsize_display) != winSize[0] || uint32_t(new_ysize_display) != winSize[1]) {
+				winSize = osl::uvec2(new_xsize_display, new_ysize_display);
+				glViewport(0, 0, winSize[0], winSize[1]);
+			}
 		}
 
 
